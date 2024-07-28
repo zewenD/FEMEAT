11cf7c500d5f296b0e84634f9007f1d06a8c3f0b
dzw <1759328812@qq.com>
2024-07-28 21:14:23 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,29 @@
+# FBDB15K
+# bash run_FEMEAT.sh 1 FBDB15K norm 0.8 0
+# bash run_FEMEAT.sh 1 FBDB15K norm 0.5 0
+# bash run_FEMEAT.sh 1 FBDB15K norm 0.2 0
+#FBYG15K
+# bash run_FEMEAT.sh 1 FBYG15K norm 0.8 0
+# bash run_FEMEAT.sh 1 FBYG15K norm 0.5 0
+# bash run_FEMEAT.sh 1 FBYG15K norm 0.2 0
+#
+#
+# FBDB15K
+# bash run_FEMEAT_il.sh 1 FBDB15K norm 0.8 0
+# bash run_FEMEAT_il.sh 1 FBDB15K norm 0.5 0
+# bash run_FEMEAT_il.sh 1 FBDB15K norm 0.2 0
+# FBYG15K
+# bash run_FEMEAT_il.sh 1 FBYG15K norm 0.8 0
+# bash run_FEMEAT_il.sh 1 FBYG15K norm 0.5 0
+bash run_FEMEAT_il.sh 1 FBYG15K norm 0.2 0
+
+
+
+
+
+
+
+
+
+
+
---
+++ run_FEMEAT_il.sh
@@ -0,0 +1,39 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python  main.py \
+            --gpu           0    \
+            --eval_epoch    1  \
+            --only_test     0   \
+            --model_name    FEMEAT \
+            --data_choice   $2 \
+            --data_split    $3 \
+            --data_rate     $4 \
+            --epoch         1000 \
+            --lr            5e-4  \
+            --hidden_units  "300,300,300" \
+            --save_model    0 \
+            --batch_size    3500 \
+            --semi_learn_step 5 \
+	        --csls          \
+	        --csls_k        3 \
+	        --random_seed   42 \
+            --exp_name      FEMEAT_sf_$5_500-1000-IL \
+            --exp_id        v1_$3_$4 \
+            --workers       12 \
+            --dist          0 \
+            --accumulation_steps 1 \
+            --scheduler     cos \
+            --attr_dim      300     \
+            --img_dim       300     \
+            --name_dim      300     \
+            --char_dim      300     \
+            --ocr_dim      300     \
+            --hidden_size   300     \
+            --tau           0.1     \
+            --structure_encoder "gat" \
+            --num_attention_heads 1 \
+            --num_hidden_layers 1 \
+            --use_surface   $5     \
+            --use_intermediate 1   \
+            --il            \
+            --w_ocr            \
+	        --il_start      500 \
+            --enable_sota \
